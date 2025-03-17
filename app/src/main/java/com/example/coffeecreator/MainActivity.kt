d8a7889870b58cb44e133e8a380527e87ce99d66
ignacy <adsa@asda.com>
2025-03-17 11:39:19 +0100

    complete "Obrazek kawy w ImageView powinien się zmieniać w zależności od wyboru kawy"

--- app/src/main/java/com/example/coffeecreator/MainActivity.kt
+++ app/src/main/java/com/example/coffeecreator/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.coffeecreator
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val coffeeRadioGroup: RadioGroup = findViewById(R.id.coffee_selection) // oznaczenie grupy radio buttonow jako zmienna coffeeRadioGroup
+
+        var selectedCoffee: String = "" // deklaracja pustej zmiennej
+
+        coffeeRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val images = when(checkedId) {
+                R.id.Espresso -> R.drawable.espresso
+                R.id.Cappuccino -> R.drawable.capuccino
+                R.id.Latte -> R.drawable.latte
+                else -> R.drawable.image_not_found
+            }
+        }
+        // TODO: fix
     }
 }
